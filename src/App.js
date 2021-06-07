ef1bb8b9750772d4d663e31d6f63a50982e6f6ec
annpermi <perminova.anna85@gmail.com>
2021-06-07 14:53:22 -0400

    Created components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Account from './components/CreateAccount';
+import Login from './components/Login';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Account />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
